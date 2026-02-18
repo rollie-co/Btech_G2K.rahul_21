243e0ade9620a0d7c19a748dfca95fce608a5b9e
Rahul Verma <rv012502@gmail.com>
2026-02-18 14:22:16 +0530

    Add files via upload

---
+++ vowel_consonent_check.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter an alphabet: ");
+    scanf(" %c", &ch);   // space before %c is important
+
+    // Check if alphabet
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+
+        // Check vowel
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+            printf("%c is a Vowel.\n", ch);
+        } else {
+            printf("%c is a Consonant.\n", ch);
+        }
+
+    } else {
+        printf("Invalid input! Please enter an alphabet.\n");
+    }
+
+    return 0;
+}
